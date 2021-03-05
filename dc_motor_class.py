f5254d6bc1291905c689d95bcb7ef0a166a70031
Rajesh Raskoti <59283233+rraskoti100@users.noreply.github.com>
2021-03-05 21:37:54 +0545

    Update dc_motor_class.py

--- dc_motor_class.py
+++ dc_motor_class.py
@@ -1,8 +1,17 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
 class DcMotor:
-  def __init__(self,ENA, In1, In2):
-    self.ENA = ENA
-    self.In1 = In1
-    self.In2 = In2
+  def __init__(self,Ena, In1A, In2A):
+    self.Ena = Ena
+    self.In1A = In1A
+    self.In2A = In2A
+    GPIO.setup(Ena, GPIO.OUT)
+    GPIO.setup(In1A, GPIO.OUT)
+    GPIO.setup(In2A, GPIO.OUT)
+    dc_pwm = GPIO.PWM(Ena, 100) # 100 is frequency
+    
   def forward():
     
 
